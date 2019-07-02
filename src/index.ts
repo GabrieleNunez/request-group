77d6f7b34948e7b2d16756a37866df45a3a7c0e2
GabrieleNunez <g_m_nunez@live.com>
2019-07-01 20:00:34 -0700

    Added export for File Request into the index

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export * from './core/base_request';
 export * from './core/base_web_request';
 export * from './core/request_manager';
 export * from './requests/web_request';
+export * from './requests/file_request';
 
 export default RequestManager;
